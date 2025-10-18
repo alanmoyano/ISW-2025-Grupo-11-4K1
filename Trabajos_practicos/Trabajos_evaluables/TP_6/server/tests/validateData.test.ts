9b80c40c79004a10c539b5a61fde7f390adb896d
Gonzalito <zavalagonzalo172@gmail.com>
2025-10-18 17:44:04 -0300

    Creacion del archivo para validacion de datos y creacion del tes que valida si la fecha seleccionada no cae en un dia festivo o un lunes

---
+++ Trabajos_practicos/Trabajos_evaluables/TP_6/server/tests/validateData.test.ts
@@ -0,0 +1,41 @@
+import app from "@server/index";
+import type { Pedido } from "@shared/types";
+import { describe, expect, it } from "vitest";
+import { validarFechaVisita } from "@server/entradasValidation";
+
+describe("Validar los datos que se cargan", () => {
+  it("La fecha no debe ser un lunes o un dia festivo", async () => {
+    const pedidoVisita: Pedido = {
+      entradas: [
+        {
+          id: 1,
+          tipoEntradaId: 2,
+          edadVisitante: 30,
+          precio: 1000,
+        },
+      ],
+      fecha: "2025-12-25",
+      idFormaDePago: 1,
+      total: 1000,
+      usuarioId: 1,
+    };
+
+    const pedidoVisita2: Pedido = {
+      entradas: [
+        {
+          id: 1,
+          tipoEntradaId: 2,
+          edadVisitante: 30,
+          precio: 1000,
+        },
+      ],
+      fecha: "2025-11-03",
+      idFormaDePago: 1,
+      total: 1000,
+      usuarioId: 1,
+    };
+
+    expect(validarFechaVisita(pedidoVisita)).toBe(false);
+    expect(validarFechaVisita(pedidoVisita2)).toBe(false);
+  });
+});
