3b38b7748666045267b1d30a0fafcbcecfddadc5
Gonzalito <zavalagonzalo172@gmail.com>
2025-10-18 17:41:26 -0300

    Agregado de tipos para entradas, formas de pago, pedidos de entradas y usuarios

--- Trabajos_practicos/Trabajos_evaluables/TP_6/shared/src/types/index.ts
+++ Trabajos_practicos/Trabajos_evaluables/TP_6/shared/src/types/index.ts
@@ -2,3 +2,35 @@ export type ApiResponse = {
   message: string;
   success: true;
 };
+
+export type TipoEntrada = {
+  id: number;
+  nombre: string;
+  precio: number;
+};
+
+export type FormaDePago = {
+  id: number;
+  nombre: string;
+};
+
+export type Entrada = {
+  id: number;
+  tipoEntradaId: number;
+  edadVisitante: number;
+  precio: number;
+};
+
+export type Pedido = {
+  usuarioId: number;
+  entradas: Array<Entrada>;
+  idFormaDePago: number;
+  fecha: string;
+  total: number;
+};
+
+export type Usuario = {
+  id: number;
+  nombre: string;
+  email: string;
+};
