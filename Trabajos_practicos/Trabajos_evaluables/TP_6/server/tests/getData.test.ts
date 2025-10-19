c69279102bff355af41b9b4505b25d3fa3197d6a
Gonzalito <zavalagonzalo172@gmail.com>
2025-10-19 19:57:39 -0300

    Implementacion de test para obtener los tipos de entrada desde la bd

--- Trabajos_practicos/Trabajos_evaluables/TP_6/server/tests/getData.test.ts
+++ Trabajos_practicos/Trabajos_evaluables/TP_6/server/tests/getData.test.ts
@@ -1,18 +1,30 @@
 import app from "@server/index";
-import { describe, expect, it } from "vitest";
+import { describe, expect, it, vi, beforeEach } from "vitest";
 import type { TipoEntrada } from "shared/src/types/index";
+import * as accesoDB from "../src/dbAccess";
+
+const mockObtenerTiposDeEntrada = vi.spyOn(accesoDB, "obtenerTiposDeEntrada");
 
 describe("Get Data test", () => {
-  it("Obtener entradas desde la base de datos", async () => {
+  // 2. Definir los datos que la BD *debe* devolver
+  const datosEsperados = [
+    { id: 1, nombre: "Regular", precio: 5000 },
+    { id: 2, nombre: "VIP", precio: 10000 },
+    { id: 3, nombre: "Menor de 3 años", precio: 0 },
+  ];
+
+  beforeEach(() => {
+    // 3. Configurar el mock para que devuelva los datos esperados
+    mockObtenerTiposDeEntrada.mockResolvedValue(datosEsperados);
+  });
+
+  it("Deberia obtener las entradas desde la base de datos a traves de ese endpoint", async () => {
     const response = await app.request("/entradas");
 
     expect(response.status).toBe(200);
     const data = await response.json();
 
-    expect(data.entradas).toEqual([
-      { id: 1, nombre: "Regular", precio: 5000 },
-      { id: 2, nombre: "VIP", precio: 10000 },
-      { id: 3, nombre: "Menor de 3 años", precio: 0 },
-    ]);
+    expect(data.message).toEqual(datosEsperados);
+    expect(data.success).toBe(true);
   });
 });
