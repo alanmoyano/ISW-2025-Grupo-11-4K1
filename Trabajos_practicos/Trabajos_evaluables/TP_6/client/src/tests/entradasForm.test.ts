00ed3feb05cb15d6ffad914df63b59b9ff64a7e5
Agustina Nievas <agusbnievas@gmail.com>
2025-10-19 18:45:46 -0300

    creacion de test de validacion del formulario de compra

---
+++ Trabajos_practicos/Trabajos_evaluables/TP_6/client/src/tests/entradasForm.test.ts
@@ -0,0 +1,49 @@
+import { expect, describe, it } from "vitest"
+import { Pedido } from "@shared/types"
+// import { entradasFormSchema } from "../lib/schemas.ts";
+
+describe("Validación del formulario de compra de entradas", () => {
+
+  describe("Fecha de visita", () => {
+
+    it("Debe requerir fecha de visita", () => {
+      const pedido: Pedido = {
+        usuarioId: 1,
+        entradas: [{ id: 1, tipoEntradaId: 1, edadVisitante: 30, precio: 1000 }],
+        idFormaDePago: 1,
+        fecha: "",
+        total: 1000,
+      };
+
+      const result = entradasFormSchema.safeParse(pedido);
+      expect(result.success).toBe(false);
+    });
+
+    it("No se puede seleccionar un lunes", () => {
+      const pedido: Pedido = {
+        usuarioId: 1,
+        entradas: [{ id: 1, tipoEntradaId: 1, edadVisitante: 30, precio: 1000 }],
+        idFormaDePago: 1,
+        fecha: "2025-10-20",
+        total: 1000,
+      };
+
+      const result = entradasFormSchema.safeParse(pedido);
+      expect(result.success).toBe(false);
+    });
+
+    it("No se puede seleccionar un día festivo", () => {
+      const pedido: Pedido = {
+        usuarioId: 1,
+        entradas: [{ id: 1, tipoEntradaId: 1, edadVisitante: 30, precio: 1000 }],
+        idFormaDePago: 1,
+        fecha: "2025-12-25",
+        total: 1000,
+        };
+
+      const result = entradasFormSchema.safeParse(pedido);
+      expect(result.success).toBe(false);
+    });
+  });
+
+});
