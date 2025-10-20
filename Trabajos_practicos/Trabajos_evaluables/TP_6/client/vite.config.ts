674dca056fc1d480bc798592cef66633571c3f71
Alan Moyano <alanmoyano203@gmail.com>
2025-10-20 10:27:02 -0300

    fix: vitest config

--- Trabajos_practicos/Trabajos_evaluables/TP_6/client/vite.config.ts
+++ Trabajos_practicos/Trabajos_evaluables/TP_6/client/vite.config.ts
@@ -1,8 +1,10 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import tailwindcss from "@tailwindcss/vite";
+/// <reference types="vitest/config" />
+
 import path from "node:path";
+import tailwindcss from "@tailwindcss/vite";
 import { tanstackRouter } from "@tanstack/router-plugin/vite";
+import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [
@@ -20,5 +22,10 @@ export default defineConfig({
       "@server": path.resolve(__dirname, "../server/src"),
       "@shared": path.resolve(__dirname, "../shared/src"),
     },
+  },
+  test: {
+    environment: "happy-dom", // para renderizar los componentes en un simulated dom (y probar el JSX)
+    setupFiles: "./src/setupTests.ts",
+    globals: true,
   },
 });
--- Trabajos_practicos/Trabajos_evaluables/TP_6/client/vitest.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "vitest/config";
-
-export default defineConfig({
-  test: {
-    environment: "happy-dom", // para renderizar los componentes en un simulated dom (y probar el JSX)
-    setupFiles: "./src/setupTests.ts",
-    globals: true,
-  },
-});
