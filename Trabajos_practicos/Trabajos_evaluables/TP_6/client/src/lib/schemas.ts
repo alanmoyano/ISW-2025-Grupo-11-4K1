96d518630185f5eb82d032e87fddda607d48da9f
Agustina Nievas <agusbnievas@gmail.com>
2025-10-21 08:51:16 -0300

    refactor: mover logica de validacón a un helper file

--- Trabajos_practicos/Trabajos_evaluables/TP_6/client/src/lib/schemas.ts
+++ Trabajos_practicos/Trabajos_evaluables/TP_6/client/src/lib/schemas.ts
@@ -1,7 +1,6 @@
 import { z } from "zod";
 import { tipoEntradaIdSchema } from "@shared/types";
-
-const diasFestivos = new Set(["2025-12-25", "2025-01-01"]);
+import { isValidFechaVisita } from "./utils";
 
 export const entradaFormSchema = z.object({
   edadVisitante: z
@@ -15,14 +14,8 @@ export const entradasFormSchema = z.object({
   fecha: z
     .string()
     .min(1, "Debe ingresar una fecha de visita")
-    .refine((value) => {
-      const date = new Date(value);
-      if (date.getUTCDay() === 1) return false;
-      if (date < new Date()) return false;
-      if (diasFestivos.has(value)) return false;
-      return true;
-    }, {
-      message: "No puede seleccionar un dia lunes o un dia festivo",
+    .refine(isValidFechaVisita, {
+      message: "No puede seleccionar un día lunes, feriado o anterior al actual",
     }),
   entradas: z
     .array(entradaFormSchema)
--- Trabajos_practicos/Trabajos_evaluables/TP_6/client/src/lib/utils.ts
+++ Trabajos_practicos/Trabajos_evaluables/TP_6/client/src/lib/utils.ts
@@ -1,6 +1,22 @@
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 
+const diasFestivos = new Set(["2025-12-25", "2025-01-01"]);
+
+export const isValidFechaVisita = (value: string): boolean => {
+  const date = new Date(value);
+  const today = new Date();
+
+  date.setHours(0, 0, 0, 0);
+  today.setHours(0, 0, 0, 0);
+
+  const esLunes = date.getUTCDay() === 1;
+  const esPasada = date < today;
+  const esFestivo = diasFestivos.has(value);
+
+  return !(esLunes || esPasada || esFestivo);
+};
+
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
